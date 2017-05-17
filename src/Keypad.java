d22088fab3599db7d7199791bafcc4454ab6bc78
Tillie Weiss <tillieweiss2001@gmail.com>
2017-05-16 22:40:10 -0700

    start of kepyad GUI

---
+++ src/Keypad.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by Isabelle on 5/16/2017.
+ */
+public class Keypad {
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+}
